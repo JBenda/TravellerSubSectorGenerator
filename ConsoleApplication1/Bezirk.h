e88131e999a8c7baf62217b06c7f9af067daa1e4
JBenda <Zabuza98@gmx.net>
2018-01-31 23:09:22 +0100

    Selecttebel Hex Grid Implimented

--- ConsoleApplication1/Bezirk.h
+++ ConsoleApplication1/Bezirk.h
@@ -10,6 +10,7 @@ public:
 	std::shared_ptr<System> getSystemAt(int x, int y);
 	std::string getSystemCode(std::string systemName);
 	std::string getSystemCode(int x, int y);
+	bool isSystem(int x, int y) { return systems.count(y * dim[1] + x) != 0; }
 private:
 	std::string generateSystemCode(std::shared_ptr<System> sys, int x, int y);
 	std::map<int,std::shared_ptr<System>> systems;
