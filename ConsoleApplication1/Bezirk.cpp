598948d439861d10eec5bad181dfb6ce38a7201d
Julian Benda <Zabuza98@gmx.net>
2018-02-01 11:46:41 -0800

    Start build TradePath

--- ConsoleApplication1/Bezirk.cpp
+++ ConsoleApplication1/Bezirk.cpp
@@ -15,6 +15,8 @@ Bezirk::Bezirk(int x, int y, int wmP)
 		}
 	}
 	std::cout << "Anzal der System " << numSystems << std::endl;
+
+	calculateRouts();
 	//calculate Traderouts
 	// if(d <= 4) && path(<=2) && a e (In, Hi, Di, Re) && b e (As, Wue, Ei, Ni, Agr, Gar, Wa)
 	//calculate Kommunikationsrouts
@@ -23,6 +25,73 @@ Bezirk::Bezirk(int x, int y, int wmP)
 	//paer hand
 	//set Bases
 	//perhand
+}
+const float da = PI / 3.f;
+#include "SFML\Graphics.hpp"
+int Bezirk::dis(int pos1, int pos2)
+{
+	sf::Vector2i p1, d;
+	p1 = { pos1 % dim[0], pos1 / dim[0]};
+	d = { pos2 % dim[0], pos2 / dim[0] };
+	d = p1 - d;
+	//BIg Hexagon and boundary check //TODO
+}
+
+void Bezirk::calculateRouts()
+{
+	map = (uint8_t*)malloc(dim[0] * dim[1] * sizeof(uint8_t));
+	memset(map, 0, dim[0] * dim[1] * sizeof(uint8_t));
+	std::map<int, std::shared_ptr<System>>::iterator itr[2];
+	int d;
+	int row = 0;
+	int colum = 0;
+	int numSystems = systems.size();
+	std::cout << "Size = " << numSystems << std::endl;
+	for (itr[0] = systems.begin(), row = 0; itr[0] != systems.end(); ++itr[0], ++row)
+	{
+		for (itr[1] = itr[0], itr[1] ++, colum = 0; itr[1] != systems.end(); ++itr[1], ++colum)
+		{
+			if ((d = dis(itr[0]->first, itr[1]->first)) <= 2)
+			{
+				map[row * numSystems + colum] = d;
+				map[colum * numSystems + row] = d;	//symetrie
+			}
+		}
+	}
+	bool updated;
+	do {
+		updated = false;
+		for (int j = 0; j < numSystems; ++j)
+		{
+			for (int i = j + 1; i < numSystems; ++i)
+			{
+				if (map[j * numSystems + i] != 0)	//Systems allreday conected
+					continue;
+				d = -1;
+				int node = -1;
+				int buff[2];
+				for (int k = 0; k < numSystems; ++k)
+				{
+					if ((buff[0] = map[j * numSystems + k]) != 0 && (buff[1] = map[i * numSystems + k]) != 0)
+					{
+						if (node < 0 || (buff[0] += buff[1]) < d)
+						{
+							node = k;
+							d = buff[0];
+						}
+					}
+				}
+				if (node >= 0)
+				{
+					map[i * numSystems + j] = d;
+					map[j * numSystems + i] = d;
+					updated = true;
+				}
+			}
+		}
+	} while (updated);
+	for (int i = 0; i < numSystems * numSystems; ++i)
+		std::cout << map[i] << (i % numSystems == 0 ? '\n' : ' ');
 }
 
 std::shared_ptr<System> Bezirk::getSystemAt(int x, int y)
--- ConsoleApplication1/Bezirk.h
+++ ConsoleApplication1/Bezirk.h
@@ -2,6 +2,8 @@
 
 #include "System.h"
 
+#define PI 3.14159265
+
 class Bezirk
 {
 public:
@@ -12,8 +14,11 @@ public:
 	std::string getSystemCode(int x, int y);
 	bool isSystem(int x, int y) { return systems.count(y * dim[1] + x) != 0; }
 private:
+	void calculateRouts();
+	int dis(int pos1, int pos2);
 	std::string generateSystemCode(std::shared_ptr<System> sys, int x, int y);
 	std::map<int,std::shared_ptr<System>> systems;
 	std::string name;
 	int dim[2];
+	uint8_t *map;
 };
